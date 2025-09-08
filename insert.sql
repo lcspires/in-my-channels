03237c547444688916c8fe8b63e369cbb41993d8
Lucas Pires <lucasopf95@gmail.com>
2025-09-07 22:15:40 -0300

    07set

--- insert.sql
+++ insert.sql
@@ -1,7 +1,1 @@
--- INSERT INTO videos (canal, titulo) VALUES ('Canal WINchester', 'Nova build do Windows 11 traz I.A. do jeito CERTO')
-
-/*
-UPDATE videos
-SET titulo = 'Aula 10: Criando dashboards avançados'
-WHERE canal = 'Gabriel Lima Gomes' AND titulo = 'Aula 10: Criando dashboards interativos'
-*/
+--INSERT INTO videos (canal, titulo) VALUES ('Carlos Nogueira DevOps', 'Tutorial de como configurar de forma local o OpenSearch - OpenSearch : Getting Started')
